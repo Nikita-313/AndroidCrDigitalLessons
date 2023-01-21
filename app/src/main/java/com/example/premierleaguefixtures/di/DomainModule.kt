f0a94cd52ebd0aac5e059249cec3d77f6228c5fc
Nikita <vorobeu224@mail.ru>
2023-01-21 12:22:17 +0300

    Hilt fix

--- app/src/main/java/com/example/premierleaguefixtures/di/DomainModule.kt
+++ app/src/main/java/com/example/premierleaguefixtures/di/DomainModule.kt
@@ -9,26 +9,31 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DomainModule {
     @Provides
+    @Singleton
     fun  provideLoadMatchesUseCase(networkMatchesRepository: NetworkMatchesRepository,saveMatchesUseCase: SaveMatchesUseCase,@ApplicationContext context: Context) : LoadMatchesUseCase {
         return LoadMatchesUseCase(networkMatchesRepository = networkMatchesRepository, saveMatchesUseCase = saveMatchesUseCase, context = context)
     }
 
     @Provides
+    @Singleton
     fun  provideSaveMatchesUseCase(localMatchesRepository: LocalMatchesRepository): SaveMatchesUseCase {
         return SaveMatchesUseCase(localMatchesRepository = localMatchesRepository)
     }
 
     @Provides
+    @Singleton
     fun  provideSearchMatchesByTeamNameUseCase(localMatchesRepository: LocalMatchesRepository) : GetMatchesByTeamNameUseCase {
         return GetMatchesByTeamNameUseCase(localMatchesRepository = localMatchesRepository)
     }
 
     @Provides
+    @Singleton
     fun  provideGetMatchByNumberUseCase(localMatchesRepository: LocalMatchesRepository) : GetMatchByNumberUseCase {
         return GetMatchByNumberUseCase(localMatchesRepository = localMatchesRepository)
     }
